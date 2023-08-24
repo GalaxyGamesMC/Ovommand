fc14ce3f3f36a0534211151f03f9baa3574dae2a
Arie <nttispay@gmail.com>
2023-08-25 03:55:06 +0700

    add BaseParameter

---
+++ src/galaxygames/ovommand/parameter/BaseParameter.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace galaxygames\ovommand;
+
+use galaxygames\ovommand\enum\ExceptionMessage;
+use galaxygames\ovommand\exception\ParameterException;
+use galaxygames\ovommand\parameter\type\ParameterTypes;
+use pocketmine\network\mcpe\protocol\types\command\CommandParameter;
+
+abstract class BaseParameter{
+    /** @var CommandParameter parameterData */
+    protected CommandParameter $parameterData;
+    protected int $flag = 0;
+    protected ParameterTypes $parameterTypes;
+
+    public function __construct(
+        protected string $name,
+        protected bool $optional = false,
+        int $flag = 0,
+    ) {
+        $this->setFlag($flag);
+        $this->parameterData = CommandParameter::standard($this->name, $this->getNetworkType()->value(), $this->flag, $this->optional);
+    }
+
+    public function getName() : string{
+        return $this->name;
+    }
+
+    public function isOptional() : bool{
+        return $this->optional;
+    }
+
+    abstract public function getNetworkType() : ParameterTypes;
+
+    public function setFlag(int $flag) : void{
+        $this->flag = match($flag) {
+            0, 1 => $flag,
+            default => throw new ParameterException(ExceptionMessage::MSG_PARAMETER_INVALID_FLAG->getErrorMessage(['flag' => $flag]), ParameterException::PARAMETER_INVALID_FLAG_ERROR)
+        };
+    }
+
+    public function getParameterData() : CommandParameter{
+        return $this->parameterData;
+    }
+}
