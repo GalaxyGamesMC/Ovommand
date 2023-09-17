682c29c3a856c7eb008d782a2fb8842cb6f0f08b
Arie <nttispay@gmail.com>
2023-09-17 21:32:32 +0700

    tabs2space

--- src/galaxygames/ovommand/enum/EnumManager.php
+++ src/galaxygames/ovommand/enum/EnumManager.php
@@ -8,41 +8,41 @@ use pocketmine\player\GameMode;
 use pocketmine\utils\SingletonTrait;
 
 final class EnumManager{
-	use SingletonTrait;
+    use SingletonTrait;
 
-	/**
-	 * @var HardEnum[]
-	 * @phpstan-var array<string, HardEnum>
-	 */
-	private array $hardEnums = [];
+    /**
+     * @var HardEnum[]
+     * @phpstan-var array<string, HardEnum>
+     */
+    private array $hardEnums = [];
     /**
      * @var SoftEnum[]
      * @phpstan-var array<string, SoftEnum>
      */
-	private array $softEnums = [];
+    private array $softEnums = [];
 
-	public function __construct(){
-		self::setInstance($this);
-		$this->setup();
-	}
+    public function __construct(){
+        self::setInstance($this);
+        $this->setup();
+    }
 
-	protected function setup() : void{
-		$this->register(new HardEnum(DefaultEnums::BOOLEAN->value, ["true" => true, "false" => false]));
+    protected function setup() : void{
+        $this->register(new HardEnum(DefaultEnums::BOOLEAN->value, ["true" => true, "false" => false]));
         $this->register(new HardEnum(DefaultEnums::GAMEMODE->value, GameMode::getAll(),
             ["survival" => "0", "creative" => "1", "adventure" => "2", "spectator" => "3"],
             ["survival" => "s", "creative" => "c", "adventure" => "a", "spectator" => "v"]));
         $this->register(new SoftEnum(DefaultEnums::ONLINE_PLAYER->value)); // Ideas: event-based closure?
-	}
+    }
 
-	public function register(SoftEnum|HardEnum $enum, bool $overwrite = false) : void{
-		$enumName = $enum->getName();
-		if (trim($enumName) === '') {
-			throw new EnumException(ExceptionMessage::MSG_ENUM_EMPTY_NAME->getRawErrorMessage(), EnumException::ENUM_EMPTY_NAME_ERROR);
-		}
+    public function register(SoftEnum|HardEnum $enum, bool $overwrite = false) : void{
+        $enumName = $enum->getName();
+        if (trim($enumName) === '') {
+            throw new EnumException(ExceptionMessage::MSG_ENUM_EMPTY_NAME->getRawErrorMessage(), EnumException::ENUM_EMPTY_NAME_ERROR);
+        }
 
-		if (!$overwrite && (isset($this->hardEnums[$enumName]) || isset($this->softEnums[$enumName]))) {
-			throw new EnumException(ExceptionMessage::MSG_ENUM_FAILED_OVERLAY->getErrorMessage(["enumName" => $enumName]), EnumException::ENUM_FAILED_OVERLAY_ERROR);
-		}
+        if (!$overwrite && (isset($this->hardEnums[$enumName]) || isset($this->softEnums[$enumName]))) {
+            throw new EnumException(ExceptionMessage::MSG_ENUM_FAILED_OVERLAY->getErrorMessage(["enumName" => $enumName]), EnumException::ENUM_FAILED_OVERLAY_ERROR);
+        }
         if ($enum instanceof SoftEnum) {
             if (isset($this->hardEnums[$enumName])) {
                 throw new EnumException(ExceptionMessage::MSG_DUPLICATED_NAME_IN_OTHER_TYPE->getErrorMessage(["enumName" => $enumName, "enumType" => HardEnum::class]), EnumException::ENUM_DUPLICATED_NAME_IN_OTHER_TYPE_ERROR);
@@ -55,7 +55,7 @@ final class EnumManager{
             }
             $this->hardEnums[$enum->getName()] = $enum;
         }
-	}
+    }
 
     public function getSoftEnum(string|DefaultEnums $enumName) : ?SoftEnum{
         if ($enumName instanceof DefaultEnums) {
@@ -74,10 +74,10 @@ final class EnumManager{
 //    /**
 //     * @deprecated Old API, use either getSoftEnum or getHardEnum for your type
 //     */
-	public function getEnum(string|DefaultEnums $enumName) {
-		if ($enumName instanceof DefaultEnums) {
-			$enumName = $enumName->value;
-		}
-		return $this->enums[$enumName] ?? null;
-	}
+    public function getEnum(string|DefaultEnums $enumName) {
+        if ($enumName instanceof DefaultEnums) {
+            $enumName = $enumName->value;
+        }
+        return $this->enums[$enumName] ?? null;
+    }
 }
