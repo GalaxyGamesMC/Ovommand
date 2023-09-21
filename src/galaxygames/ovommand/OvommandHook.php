05bc0b59ecfd77d08b4161fb5b898a593f608dde
Arie <nttispay@gmail.com>
2023-09-22 00:37:38 +0700

    update OvommandHook.php

--- src/galaxygames/ovommand/OvommandHook.php
+++ src/galaxygames/ovommand/OvommandHook.php
@@ -6,8 +6,10 @@ namespace galaxygames\ovommand;
 use galaxygames\ovommand\enum\DefaultEnums;
 use galaxygames\ovommand\enum\EnumManager;
 use galaxygames\ovommand\fetus\IParametable;
+use galaxygames\ovommand\parameter\BaseParameter;
 use galaxygames\ovommand\syntax\SyntaxConst;
 use muqsit\simplepackethandler\SimplePacketHandler;
+use pocketmine\command\CommandSender;
 use pocketmine\event\EventPriority;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
@@ -24,29 +26,12 @@ final class OvommandHook{
 	protected static PluginBase $plugin;
 	protected static EnumManager $enumManager;
 
-	//    private static array $parameterAttributes = [];
-	//    private static array $permissions = [];
-
 	public static function register(PluginBase $plugin) : bool{
 		if (!self::$registered || self::$plugin === null || !self::$plugin->isEnabled()) {
 			self::$enumManager = EnumManager::getInstance();
 			$server = $plugin->getServer();
 			$pluginManager = $server->getPluginManager();
-			//$pluginManager = Server::getInstance()->getPluginManager();
 			try {
-				//                foreach ($server->getCommandMap()->getCommands() as $name => $command) {
-				//                    if ($command instanceof BaseCommand) {
-				//                        $ref = new ReflectionClass($command);
-				//                        foreach ($ref->getAttributes(CommandParameters::class) as $attribute) {
-				//                            //TODO: /** @var CommandAttribute $attribute */
-				//                            $attribute = $attribute->newInstance();
-				//
-				//                            $this->parameters[$name][] = $parameters->hasSoftEnum() ? $parameters : $parameters->encode();
-				//                            $this->permissions[$name][] = $parameters->getPermission();
-				//                        }
-				//                    }
-				//                }
-
 				$pluginManager->registerEvent(PlayerJoinEvent::class, function(PlayerJoinEvent $event){
 					$enum = self::$enumManager->getSoftEnum(DefaultEnums::ONLINE_PLAYER);
 					$enum?->addValue($event->getPlayer()->getName());
@@ -63,31 +48,15 @@ final class OvommandHook{
 						return;
 					}
 					foreach ($packet->commandData as $name => $commandData) {
-						if (!isset(self::$parameterAttributes[$name])) {
-							continue;
-						}
 						$command = Server::getInstance()->getCommandMap()->getCommand($name);
 						if ($command instanceof BaseCommand) {
 							foreach ($command->getConstraints() as $constraint) {
-								//TODO: Add constraints
+								if (!$constraint->isVisibleTo($player)) {
+									continue 2;
+								}
 							}
-							$commandData->overloads = self::generateOverloads($command);
-							continue;
+							$commandData->overloads = self::generateOverloads($player, $command);
 						}
-						//                        $newOverloads = [];
-						//                        foreach (self::$parameters[$name] as $index => $parameter) {
-						//                            $permission = self::$permissions[$name][$index];
-						//                            if ($permission !== null && !$player->hasPermission($permission)) {
-						//                                continue;
-						//                            }
-						////                            if (is_array($parameter)) {
-						////                                foreach ($parameter as $p) {
-						////                                    $newOverloads[] = new CommandOverload(false, $p instanceof CommandParameters ? $p->encode() : [$p]);
-						////                                }
-						////                            }
-						//                            $newOverloads[] = new CommandOverload(false, $parameter instanceof CommandParameters ? $parameter->encode() : $parameter);
-						//                        }
-						//                        $commandData->overloads = $newOverloads;
 					}
 				});
 			} catch (\ReflectionException $e) {
@@ -96,33 +65,30 @@ final class OvommandHook{
 			}
 			self::$plugin = $plugin;
 			self::$registered = true;
+			return true;
 		}
 		return false;
 	}
 
 	/**
-	 * @param BaseCommand $command
+	 * @param CommandSender $sender
+	 * @param BaseCommand   $command
 	 *
 	 * @return CommandOverload[]
 	 */
-	private static function generateOverloads(BaseCommand $command) : array{
+	private static function generateOverloads(CommandSender $sender, BaseCommand $command) : array{
 		$overloads = [];
 
 		foreach ($command->getSubCommands() as $label => $subCommand) {
-			if (!$subCommand->testPermissionSilent(...) || $subCommand->getName() !== $label) { // hide aliases
+			if ($subCommand->isAliases($label) || !$subCommand->testPermissionSilent($sender)) { //Get origin label
 				continue;
 			}
 			foreach ($subCommand->getConstraints() as $constraint) {
-				if (!$constraint->isVisibleTo($cs)) {
+				if (!$constraint->isVisibleTo($sender)) {
 					continue 2;
 				}
 			}
-			$scParam = new CommandParameter();
-			$scParam->paramName = $label;
-			$scParam->paramType = AvailableCommandsPacket::ARG_FLAG_VALID | AvailableCommandsPacket::ARG_FLAG_ENUM;
-			$scParam->isOptional = false;
-			$scParam->enum = new CommandEnum($label, [$label]);
-
+			$scParam = CommandParameter::enum($subCommand->getName(), new CommandEnum("enum#" . spl_object_id($subCommand), [$label, ...$subCommand->getShowAliases()]), 0);
 			$overloadList = self::generateOverloadList($subCommand);
 			if (!empty($overloadList)) {
 				foreach ($overloadList as $overload) {
@@ -141,52 +107,20 @@ final class OvommandHook{
 	}
 
 	private static function generateOverloadList(IParametable $parametable) : array{
-		$input = $parametable->getParameterList();
 		$combinations = [];
-		$outputLength = array_product(array_map("count", $input));
-		$indexes = [];
-		foreach ($input as $k => $charList) {
-			$indexes[$k] = 0;
+		foreach ($parametable->getParameterList() as $parameters) {
+//			/** @var CommandParameter[] $params */
+//			$params = [];
+//			foreach ($parameters as $parameter) {
+//				$params[] = $parameter->getNetworkParameterData();
+//			}
+//			$combinations[] = new CommandOverload(false, $params);
+
+			$combinations[] = new CommandOverload(false, array_map(static fn(BaseParameter $parameter) : CommandParameter => $parameter->getNetworkParameterData(), $parameters));
 		}
-		do {
-			/** @var CommandParameter[] $set */
-			$set = [];
-			foreach ($indexes as $k => $index) {
-				//                $param = $set[$k] = clone $input[$k][$index]->getNetworkParameterData();
-
-				if (isset($param->enum) && $param->enum instanceof CommandEnum) {
-					$refClass = new \ReflectionClass(CommandEnum::class);
-					//					$refProp = $refClass->getProperty("enumName");
-					//					$refProp->setAccessible(true);
-					//					$refProp->setValue($param->enum, "enum#" . spl_object_id($param->enum));
-				}
-			}
-			$combinations[] = new CommandOverload(false, $set);
-
-			foreach ($indexes as $k => $v) {
-				$indexes[$k]++;
-				$lim = count($input[$k]);
-				if ($indexes[$k] >= $lim) {
-					$indexes[$k] = 0;
-					continue;
-				}
-				break;
-			}
-		} while (count($combinations) !== $outputLength);
 
 		return $combinations;
 	}
-
-	//    private static function solveAttributeCommand(string $name, Command $command) : void{
-	//        $ref = new ReflectionClass($command);
-	//        foreach ($ref->getAttributes(CommandParameters::class) as $attribute) {
-	//            /** @var CommandParameters $parameters */
-	//            $parameters = $attribute->newInstance();
-	//
-	//            self::$parameters[$name][] = $parameters->hasSoftEnum() ? $parameters : $parameters->encode();
-	//            self::$permissions[$name][] = $parameters->getPermission();
-	//        }
-	//    }
 
 	public static function isRegistered() : bool{
 		return self::$registered;
