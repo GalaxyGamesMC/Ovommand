c414d38b7600cb66537ae61b4f261838fe32b1ce
Arie <nttispay@gmail.com>
2023-09-22 10:29:35 +0700

    choose to use the Beta system

--- src/galaxygames/ovommand/BaseCommand.php
+++ src/galaxygames/ovommand/BaseCommand.php
@@ -1,61 +1,28 @@
 <?php
 declare(strict_types=1);
 
-namespace galaxygames\ovommand;
+namespace galaxygames\ovommand\fetus\beta;
 
-use galaxygames\ovommand\constraint\BaseConstraint;
-use galaxygames\ovommand\fetus\IParametable;
-use galaxygames\ovommand\fetus\OvommandTrait;
 use galaxygames\ovommand\fetus\ParametableTrait;
-use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\lang\Translatable;
 use pocketmine\permission\Permission;
 use pocketmine\plugin\Plugin;
 use pocketmine\plugin\PluginOwned;
 
-abstract class BaseCommand extends Command implements IParametable, PluginOwned{
-	use OvommandTrait;
+abstract class BaseCommand extends Ovommand implements PluginOwned{
 	use ParametableTrait;
 
 	public function __construct(protected Plugin $plugin, string $name, Translatable|string $description = "", array $aliases = [], Permission|string|array $permission = null){
-		parent::__construct($name, $description, null, $aliases);
-
-		$this->prepare();
-
-		if ($permission !== null) {
-			$this->setPermission($permission);
-		}
+		parent::__construct($name, $description, $aliases);
 	}
 
 	/**
 	 * @param CommandSender                   $sender
-	 * @param string                          $aliasUsed
+	 * @param string                          $label
 	 * @param array|array<string,mixed|array> $args
 	 */
-	abstract public function onRun(CommandSender $sender, string $aliasUsed, array $args) : void;
-
-	/**
-	 * @return BaseSubCommand[]
-	 */
-	public function getSubCommands() : array{
-		return $this->subCommands;
-	}
-
-	public function addConstraint(BaseConstraint $constraint) : void{
-		$this->constraints[] = $constraint;
-	}
-
-	/**
-	 * @return BaseConstraint[]
-	 */
-	public function getConstraints() : array{
-		return $this->constraints;
-	}
-
-	public function getUsageMessage() : string{
-		return $this->getUsage();
-	}
+	abstract public function onRun(CommandSender $sender, string $label, array $args) : void;
 
 	public function getOwningPlugin() : Plugin{
 		return $this->plugin;
--- src/galaxygames/ovommand/BaseSubCommand.php
+++ src/galaxygames/ovommand/BaseSubCommand.php
@@ -1,20 +1,13 @@
 <?php
 declare(strict_types = 1);
 
-namespace galaxygames\ovommand;
+namespace galaxygames\ovommand\fetus\beta;
 
-use galaxygames\ovommand\fetus\IParametable;
-use galaxygames\ovommand\fetus\OvommandTrait;
 use galaxygames\ovommand\fetus\ParametableTrait;
-use pocketmine\command\CommandSender;
-use pocketmine\lang\KnownTranslationFactory;
 use pocketmine\lang\Translatable;
-use pocketmine\permission\PermissionManager;
 use pocketmine\plugin\Plugin;
-use pocketmine\utils\TextFormat;
 
-abstract class BaseSubCommand implements IParametable{
-	use OvommandTrait;
+abstract class BaseSubCommand extends Ovommand{
 	use ParametableTrait;
 	protected Translatable|string $usageMessage;
 
@@ -26,8 +19,10 @@ abstract class BaseSubCommand implements IParametable{
 	/** @var string[] */
 	protected array $permissions = [];
 	private ?string $permissionMessage = null;
+	protected Ovommand $parent;
 
-	public function __construct(protected string $name, protected string $description = "", array $hiddenAliases = [], array $showAliases = []){
+	public function __construct(protected string $name, protected string|Translatable $description = "", array $hiddenAliases = [], array $showAliases = []){
+		parent::__construct($name, $this->description);
 		$this->hiddenAliases = array_unique($hiddenAliases);
 		$this->showAliases = array_unique($showAliases);
 			//$this->generateUsageMessage();
@@ -46,74 +41,6 @@ abstract class BaseSubCommand implements IParametable{
 		return in_array($in, $this->showAliases, true);
 	}
 
-	public function getName() : string{
-		return $this->name;
-	}
-
-	public function getDescription() : string{
-		return $this->description;
-	}
-
-	public function getUsageMessage() : string{
-		return $this->usageMessage;
-	}
-
-	public function getPermissions() : array{
-		return $this->permissions;
-	}
-
-	public function setPermissions(array $permissions) : void{
-		$permissionManager = PermissionManager::getInstance();
-		foreach ($permissions as $perm) {
-			if ($permissionManager->getPermission($perm) === null) {
-				throw new \InvalidArgumentException("Cannot use non-existing permission \"$perm\"");
-			}
-		}
-		$this->permissions = $permissions;
-	}
-
-	public function setPermission(string $permission) : void{
-		$permissionManager = PermissionManager::getInstance();
-		if ($permissionManager->getPermission($permission) === null) {
-			throw new \InvalidArgumentException("Cannot use non-existing permission \"$permission\"");
-		}
-		$this->permissions[] = $permission;
-	}
-
-	public function testPermissionSilent(CommandSender $sender) : bool{
-		foreach ($this->permissions as $permission) {
-			if ($sender->hasPermission($permission)) {
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	/**
-	 * @internal Used to pass the parent context from the parent command
-	 */
-	public function setParent(BaseSubCommand|BaseCommand $parent) : self{
-		$this->parent = $parent;
-		return $this;
-	}
-
-	public function sendError(int $errorCode, array $args = []) : void{
-		$this->parent->sendError($errorCode, $args);
-	}
-
-	public function sendUsage() : void{
-		$this->currentSender->sendMessage("/{$this->parent->getName()} $this->usageMessage");
-	}
-
-	public function getOwningPlugin() : Plugin{
-		$parent = $this->getParent();
-		while ($parent instanceof self) {
-			$parent = $parent->getParent();
-		}
-		return $parent->getOwningPlugin();
-	}
-
 	public function getHiddenAliases() : array{
 		return $this->hiddenAliases;
 	}
@@ -122,17 +49,23 @@ abstract class BaseSubCommand implements IParametable{
 		return $this->showAliases;
 	}
 
-	public function testPermission(CommandSender $target, ?string $permission = null) : bool{
-		if($this->testPermissionSilent($target)){
-			return true;
-		}
+	public function getParent() : Ovommand{
+		return $this->parent;
+	}
 
-		if($this->permissionMessage === null){
-			$target->sendMessage(KnownTranslationFactory::pocketmine_command_error_permission($this->name)->prefix(TextFormat::RED));
-		}elseif($this->permissionMessage !== ""){
-			$target->sendMessage(str_replace("<permission>", $permission ?? implode(";", $this->permissions), $this->permissionMessage));
-		}
+	/**
+	 * @internal Used to pass the parent context from the parent command
+	 */
+	public function setParent(Ovommand $parent) : self{
+		$this->parent = $parent;
+		return $this;
+	}
 
-		return false;
+	public function getOwningPlugin() : Plugin{
+		$parent = $this->getParent();
+		while ($parent instanceof BaseSubCommand) {
+			$parent = $parent->getParent();
+		}
+		return $parent->getOwningPlugin();
 	}
 }
