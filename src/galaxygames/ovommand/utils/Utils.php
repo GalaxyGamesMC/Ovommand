653f82be44e6aaa9ba3fccd8c331f2b615b282e1
Arie <nttispay@gmail.com>
2023-09-30 15:41:14 +0700

    subCommand now calls the right method

--- src/galaxygames/ovommand/utils/Utils.php
+++ src/galaxygames/ovommand/utils/Utils.php
@@ -11,7 +11,7 @@ class Utils{
 	public static function parseUsages(BaseCommand $command) : string{
 		$usages = ["/" . $command->generateUsageMessage()];
 		foreach ($command->getSubCommands() as $subCommand) {
-			$usages[] = $subCommand->getUsageMessage();
+			$usages[] = $subCommand->getUsage();
 		}
 		$usages = array_unique($usages);
 		return implode("\n - /" . $command->getName() . " ", $usages);
