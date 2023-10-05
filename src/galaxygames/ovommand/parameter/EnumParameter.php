0777c9cd7bb5f2813e31acc741fe467d504b9761
Arie <nttispay@gmail.com>
2023-10-05 12:02:10 +0700

    remove PlaceholderEnum.php and its related method

--- src/galaxygames/ovommand/parameter/EnumParameter.php
+++ src/galaxygames/ovommand/parameter/EnumParameter.php
@@ -5,7 +5,7 @@ namespace galaxygames\ovommand\parameter;
 
 use galaxygames\ovommand\enum\DefaultEnums;
 use galaxygames\ovommand\enum\EnumManager;
-use galaxygames\ovommand\enum\PlaceholderEnum;
+use galaxygames\ovommand\exception\EnumException;
 use galaxygames\ovommand\parameter\result\BrokenSyntaxResult;
 use galaxygames\ovommand\parameter\result\ValueResult;
 use pocketmine\network\mcpe\protocol\types\command\CommandParameter;
@@ -15,12 +15,12 @@ use shared\galaxygames\ovommand\fetus\IEnum;
 use shared\galaxygames\ovommand\fetus\IStaticEnum;
 
 class EnumParameter extends BaseParameter{
-	protected PlaceholderEnum|IEnum $enum;
+	protected ?IEnum $enum;
 
 	public function __construct(string $name, DefaultEnums|string $enumName, bool $isSoft = false, bool $optional = false, int $flag = 0, protected bool $returnRaw = false){
-		$this->enum = EnumManager::getInstance()->getEnum($enumName, $isSoft) ?? PlaceholderEnum::create($name, $isSoft);
-		if ($this->enum instanceof PlaceholderEnum) {
-			var_dump($this->enum);
+		$this->enum = EnumManager::getInstance()->getEnum($enumName, $isSoft);
+		if ($this->enum === null) {
+			throw new EnumException("LOL UNKNOWN ENUM!"); //TODO: Enum, code etc
 		}
 		parent::__construct($name, $optional, $flag);
 	}
@@ -37,23 +37,11 @@ class EnumParameter extends BaseParameter{
 		return match (true) {
 			$this->enum instanceof IStaticEnum => false,
 			$this->enum instanceof IDynamicEnum => true,
-			$this->enum instanceof PlaceholderEnum => $this->enum->isSoft(),
 			default => throw new \RuntimeException("TODO") //TODO: Update msg
 		};
 	}
 
-	protected function resolvePlaceholder() : void{
-		if ($this->enum instanceof PlaceholderEnum) {
-			$enum = EnumManager::getInstance()->getEnum($this->getValueName(), $this->isSoft());
-			if ($enum === null) {
-				throw new \RuntimeException("Enum was not registered!"); //TODO: Better name
-			}
-			$this->enum = $enum;
-		}
-	}
-
 	public function parse(array $parameters) : BaseResult{
-		$this->resolvePlaceholder();
 		$enumValue = $this->enum->getValue($key = implode(" ", $parameters));
 		if ($enumValue !== null) {
 			return ValueResult::create($this->returnRaw ? $key : $enumValue); //TODO: Best sol?
@@ -62,7 +50,6 @@ class EnumParameter extends BaseParameter{
 	}
 
 	public function getNetworkParameterData() : CommandParameter{
-		$this->resolvePlaceholder();
 		return CommandParameter::enum($this->name, $this->enum->encode(), $this->flag, $this->optional);
 	}
 
