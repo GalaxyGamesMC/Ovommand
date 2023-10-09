bf7d4e84d04931b102d5c476fc6c315cfd095b9b
Arie <nttispay@gmail.com>
2023-10-09 15:00:05 +0700

    eh

--- test/array_diff_test.php
+++ test/array_diff_test.php
@@ -1,4 +1,31 @@
 <?php
+/**
+ * Ovommand - A command virion for PocketMine-MP
+ *                     ／l、
+ *                   （ﾟ､ ｡ ７
+ *                     l ~ヽ
+ *  _____             じしf_,)ノ	               _
+ * |     | _ _  ___  _____  _____  ___  ___  _| |
+ * |  |  || | || . ||     ||     || .'||   || . |
+ * |_____| \_/ |___||_|_|_||_|_|_||__,||_|_||___|
+ *
+ * Copyright (C) 2023 GalaxyGamesMC
+ * @link https://github.com/GalaxyGamesMC/Ovommand
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, @see <https://www.gnu.org/licenses/>.
+ */
+
 declare(strict_types=1);
 
 $values1 = [
