88d2c791aef9b8c2be61eb4146684eb2fa54fc72
Arie <nttispay@gmail.com>
2023-10-24 00:56:14 +0700

    new test

---
+++ test/position_parameter3.php
@@ -0,0 +1,119 @@
+<?php
+declare(strict_types=1);
+
+require_once "D:\pmmp\Ovommand\\vendor\autoload.php";
+
+use galaxygames\ovommand\parameter\PositionParameter;
+use galaxygames\ovommand\parameter\result\BrokenSyntaxResult;
+
+$parameter = new PositionParameter("t");
+
+$test = 100000;
+//$test = 1;
+$out = $test === 1;
+
+$parameters = [
+	["~131318763183~-131873173", "~11232213123213123123213213763172791281631287162317863781.1321368763287812"],
+	["~~~"],
+	["~~", "~"],
+	["^^^a"],
+	["~131318763183~-131873173", "~1123221312321312312321321376317a2791281631287162317863781.1321368763287812"],
+	["~131318763183~-131873173", "^+11232213123213123123213213763aaa"],
+	["~13131", "~+1123221312321312312", "-13132313133"],
+	["13131", "+1123221312321312312", "-13132313133"],
+	["13131", "+1123221312321312312", "-13132313133", "~12312"],
+	["adadad~adadadada~adada adsadadad adad+133231😿😿😿😭✨💀😊~asdad adadaadsada"],
+	["~adadadada~adada adsadadad adad+133231😿😿😿😭✨💀😊~asdad adadaadsada"],
+	["~~adada adsadadad adad+133231😿😿😿😭✨💀😊~asdad adadaadsada"],
+	["~~+133231😿😿😿😭✨💀😊~asdad adadaadsada"],
+	["~~+133231~asdad adadaadsada"],
+	["~~+133231~~13132"],
+	["~~+133231               ~~13132"],
+	[""],
+	[" ", "  "],
+];
+
+$tz = 'GMT+7';
+$timestamp = time();
+try {
+	$dt = new DateTime("now", new DateTimeZone($tz));
+	$dt->setTimestamp($timestamp);
+	echo "Test with $test entries, " . count($parameters) . " inputs, " . $dt->format("h:i d/m/Y T") . "\n\n";
+} catch (Exception $e) {
+	echo $e->getMessage();
+}
+
+foreach ($parameters as $params) {
+	echo "\"" . implode(" ", $params) . "\"" . "\n";
+	$rate = 0;
+	for ($i = 1; $i <= $test; ++$i) {
+		$time = -hrtime(true);
+		$result = $parameter->parse($params);
+		$time += hrtime(true);
+		if ($time === 0) {
+			$i--;
+		}
+		$rate += $time;
+	}
+	if ($out && $result instanceof BrokenSyntaxResult) {
+		echo var_export($result->getBrokenSyntax()) . "\n";
+	}
+	echo("Test1: " . sprintf('%0.4f', $rate/$test) . PHP_EOL);
+	$rate = 0;
+	for ($i = 1; $i <= $test; ++$i) {
+		$time = -hrtime(true);
+		$result = $parameter->parse2($params);
+		$time += hrtime(true);
+		if ($time === 0) {
+			$i--;
+		}
+		$rate += $time;
+	}
+	if ($out && $result instanceof BrokenSyntaxResult) {
+		echo var_export($result->getBrokenSyntax()) . "\n";
+	}
+	echo("Test2: " . sprintf('%0.4f', $rate/$test) . PHP_EOL);
+	$rate = 0;
+	for ($i = 1; $i <= $test; ++$i) {
+		$time = -hrtime(true);
+		$result = $parameter->parse3($params);
+		$time += hrtime(true);
+		if ($time === 0) {
+			$i--;
+		}
+		$rate += $time;
+	}
+	if ($out && $result instanceof BrokenSyntaxResult) {
+		echo var_export($result->getBrokenSyntax()) . "\n";
+	}
+	echo("Test3: " . sprintf('%0.4f', $rate/$test) . PHP_EOL);
+	$rate = 0;
+	for ($i = 1; $i <= $test; ++$i) {
+		$time = -hrtime(true);
+		$result = $parameter->parse4($params);
+		$time += hrtime(true);
+		if ($time === 0) {
+			$i--;
+		}
+		$rate += $time;
+	}
+	if ($out && $result instanceof BrokenSyntaxResult) {
+		echo var_export($result->getBrokenSyntax()) . "\n";
+	}
+	echo("Test4: " . sprintf('%0.4f', $rate/$test) . PHP_EOL);
+	$rate = 0;
+	for ($i = 1; $i <= $test; ++$i) {
+		$time = -hrtime(true);
+		$result = $parameter->parse5($params);
+		$time += hrtime(true);
+		if ($time === 0) {
+			$i--;
+		}
+		$rate += $time;
+	}
+	if ($out && $result instanceof BrokenSyntaxResult) {
+		echo var_export($result->getBrokenSyntax()) . "\n";
+	}
+	echo("Test5: " . sprintf('%0.4f', $rate/$test) . PHP_EOL);
+	echo ("-----------------------------------------------------------------\n");
+}
